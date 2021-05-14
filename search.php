43d51049043fd89c78904d49c86f568b85b4587e
Steven Kim <stevenkim1989@gmail.com>
2021-05-14 15:12:40 -0700

    add clear button, change color of buttons

--- search.php
+++ search.php
@@ -5,22 +5,29 @@ include("./SearchResult.php");
 <h1>Search</h1>
 <form class="form-inline my-2 my-lg-0" style="margin:10px" method="POST" >
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="search" name="search_input">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="search">Search</button>
+      <button class="btn btn-small btn-primary" type="submit" name="search">Search</button>
+      <button class="btn btn-small btn-warning" name="clean" onClick="history.go(0);">Clear</button>
 </form>
 
 <?php
+$input = "";
 
 if(isset($_POST['search'])){ //check if form was submitted
       $input = $_POST['search_input']; //get input text
+
+      $searchResult = new SearchResult($input);
+
+      $searchResult->creatingNewsletterTable();
+
+      echo "<hr>";
+
+      $searchResult->creatingDrugTable();
+
 }
 
-$searchResult = new SearchResult($input);
-
-$searchResult->creatingNewsletterTable();
-
-echo "<hr>";
-
-$searchResult->creatingDrugTable();
+function clearInput() {
+      $input = "";
+}
 
 ?>
 
